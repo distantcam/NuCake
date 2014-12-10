fb442c56d1bdf6cf3d206e5879e1eb22308e5904
Cameron MacFarland <distantcam@gmail.com>
2014-12-10 16:34:14 +0800

    Removing custom reporter

--- src/NuCake.Tests/MSBuildTests.cs
+++ src/NuCake.Tests/MSBuildTests.cs
@@ -1,31 +1,9 @@
-﻿using System.IO;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using ApprovalTests;
-using ApprovalTests.Core;
 using ApprovalTests.Reporters;
 using Xunit;
 
-public class MyCustomReporter : IEnvironmentAwareReporter
-{
-    public bool IsWorkingInThisEnvironment(string forFile)
-    {
-        return GenericDiffReporter.IsTextFile(forFile);
-    }
-
-    public void Report(string approved, string received)
-    {
-        var o = received.Replace(".received.", ".output.");
-        File.Copy(received, o);
-
-        var a = File.Exists(approved) ? File.ReadAllText(approved) : "";
-        var r = File.ReadAllText(received);
-        QuietReporter.DisplayCommandLineApproval(approved, received);
-
-        //Assert.Equal(a, r);
-    }
-}
-
-[UseReporter(typeof(MyCustomReporter))]
+[UseReporter(typeof(DiffReporter))]
 public class MSBuildTests
 {
     [MethodImpl(MethodImplOptions.NoInlining)]
